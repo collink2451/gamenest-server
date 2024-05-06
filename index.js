7babb622679791b67534aee6cbf17581e50463c5
Collin Koldoff <collinkoldoff@gmail.com>
2024-05-06 11:07:50 -0500

    Moved auth to separate file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app = express()
 db.connect()
 dotenv.config()
 
-
+const auth = require('./auth');
 const battleship = require('./games/battleship');
 const wordle = require('./games/wordle')
 
@@ -31,7 +31,7 @@ var corsOptions = {
 app.use(cors(corsOptions))
 app.use(cookieParser());
 app.use(bodyParser.json())
-
+app.use(auth);
 
 app.use(battleship);
 app.use(wordle);
@@ -47,78 +47,8 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/static'))
 
 
-app.get('/auth/github/callback', async (req, res) => {
-  const code = req.query.code;
 
-  // Exchange code for access token
-  const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code,
-    }),
-  });
 
-  const tokenData = await tokenResponse.json();
-  const accessToken = tokenData.access_token;
-
-  // Get user details
-  const userResponse = await fetch('https://api.github.com/user', {
-    headers: {
-      Authorization: `token ${accessToken}`,
-    },
-  });
-
-  // Get username from response
-  const { login } = await userResponse.json();
-
-  if (accessToken) {
-    // Create session cookie
-    res.cookie('accessToken', accessToken, {
-      sameSite: 'None',
-      secure: true
-    })
-
-    // Return access token and user details
-    res.json({ success: true, accessToken, user: login });
-  } else {
-    res.status(204).send()
-  }
-});
-
-app.get('/auth/check', (req, res) => {
-  if (!req.cookies) {
-    res.status(204).send()
-    return;
-  }
-  const accessToken = req.cookies.accessToken;
-  if (!accessToken) {
-    res.status(204).send()
-    return;
-  }
-  // Verify access token against github
-  fetch('https://api.github.com/user', {
-    headers: {
-      Authorization: `token ${accessToken}`,
-    },
-  }).then((response) => response.json())
-    .then((data) => {
-      res.json({ success: true, accessToken, user: data.login });
-    })
-    .catch(() => {
-      res.status(204).send()
-    });
-});
-
-app.get('/auth/logout', (req, res) => {
-  res.clearCookie('accessToken');
-  res.status(204).send()
-});
 
 // The app.get functions below are being processed in Node.js running on the server.
 // Implement a custom About page.
